e6ddc6bd49991e79310279de20446c845b9ee856
Vu Bao Minh Nguyen <107901176+vubaominh@users.noreply.github.com>
2024-02-01 12:00:59 -0400

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -2,6 +2,16 @@ package lecture2_Quick_MergeSorts;
 
 import java.util.Arrays;
 
+// Merge Sort:
+// Divide and Conquer: Split halves, Sort halves, Merge sorted halves together
+// #1: [left / size - left] => Recursive merge(1/2 arr)
+// #2: merge(l, r, OGa):
+// Phan chia 2 phan keo deu
+// leftA[left] < rightA[right]) {OGa[i] = leftA[left]; }
+// else:                         OGa[i] = rightA[right]
+// I. (left < leftSize) && (right < rightSize) | II. << Rieng le
+
+
 public class MergeSort {
 public static void main(String[] args) {
 	int[] arr;
@@ -24,13 +34,14 @@ private static void mergeSort(int[] arr) {
 	int middle = len >>1;
 	
 	if(len > 1) {
-
-	int[] rightA = new int[len - middle]; // De phong so le
+	
 	int[] leftA = new int[middle];
-
-	System.arraycopy(arr, middle, rightA, 0, rightA.length);
+	int[] rightA = new int[len - middle]; // De phong so le
+	
+	// Parition sang cac left right array: 1/2
+	// .arraycopy(from, main_index, to, startIndexOfTo, length)
 	System.arraycopy(arr, 0, leftA, 0, leftA.length);
-
+	System.arraycopy(arr, middle, rightA, 0, rightA.length);
 	// Het
 
 	
